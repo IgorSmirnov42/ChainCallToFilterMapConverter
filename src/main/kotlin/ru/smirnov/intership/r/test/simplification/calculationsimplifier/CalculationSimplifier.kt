73bf6ba05b588fab1e377f3aa60c0fb7b84c7668
Igor Smirnov <piter3343@gmail.com>
2020-04-04 20:26:20 +0300

    deleted useless files

--- src/main/kotlin/ru/smirnov/intership/r/test/simplification/calculationsimplifier/CalculationSimplifier.kt
+++
@@ -1,9 +0,0 @@
-package ru.smirnov.intership.r.test.simplification.calculationsimplifier
-
-import ru.smirnov.intership.r.test.grammar.components.CallChain
-
-object CalculationSimplifier {
-    fun simplify(callChain: CallChain) {
-
-    }
-}
